7b3bff683272d23380fbfee4c1d56c1deb709b09
Kevin Hernández Rostrán <kevinah95@gmail.com>
2016-11-28 20:40:17 -0600

    Update hello-world.cpp

--- hello-world.cpp
+++ hello-world.cpp
@@ -1,13 +1,22 @@
-// my first program in C++
-#include <iostream>
-
-int main()
+// Bubble Sort Function for Descending Order 
+void BubbleSort(apvector <int> &num)
 {
-  std::cout << "Hello World!";
-}// my first program in C++
-#include <iostream>
-
-int main()
-{
-  std::cout << "Hello World!";
+      int i, j, flag = 1;    // set flag to 1 to start first pass
+      int temp;             // holding variable
+      int numLength = num.length( ); 
+      for(i = 1; (i <= numLength) && flag; i++)
+     {
+          flag = 0;
+          for (j=0; j < (numLength -1); j++)
+         {
+               if (num[j+1] > num[j])      // ascending order simply changes to <
+              { 
+                    temp = num[j];             // swap elements
+                    num[j] = num[j+1];
+                    num[j+1] = temp;
+                    flag = 1;               // indicates that a swap occurred.
+               }
+          }
+     }
+     return;   //arrays are passed to functions by address; nothing is returned
 }
